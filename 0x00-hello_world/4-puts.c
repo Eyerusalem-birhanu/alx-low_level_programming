aa656bfbafdaecbd007899007b773c8e677f1cb6
Eyerusalem-birhanu <jerrybirhanu94@gmail.com>
2022-06-02 09:05:38 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,7 @@
  *   *
  *    * Return: Always 0 (Success)
 */
-int main(void) 
-
+int main(void) first c programming 
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
