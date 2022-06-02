5280c3393bdeb3be7761c0fc16ea6124f8123a4d
Eyerusalem-birhanu <jerrybirhanu94@gmail.com>
2022-06-02 10:29:27 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /*
- * main - print the phase "with proper grammer, but the outcome is a piece of art
- * Return: 0 if excited prperly,n-zero otherwise
- */
+* main - print the phase "with proper grammer, but the outcome is a piece of art
+* Return: 0 if excited prperly,n-zero otherwise
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
