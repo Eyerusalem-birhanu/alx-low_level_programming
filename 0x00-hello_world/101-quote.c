1203c90041814453c7155f153fa19fb4c5e02cab
Eyerusalem-birhanu <jerrybirhanu94@gmail.com>
2022-06-02 10:19:33 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*
+*  * main - prints the phrase "and that piece of art is useful" - Dora Korpar, 2015-10-19
+*    * 0 if exited properly, non-zero otherwise
+*/
+int main(void)
+{
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1)
+}
