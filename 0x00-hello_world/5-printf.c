ef990096add9e21749dbba1374f45d91195d2f10
Eyerusalem-birhanu <jerrybirhanu94@gmail.com>
2022-06-02 09:25:12 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-/*
- * main - Entry point
- *
+/**
+ * main - print the phase "with proper grammer, but the outcome is a piece of art
  * Return: Always 0 (Success)
  */
 int main(void)
