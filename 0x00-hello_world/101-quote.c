bdfc649f786a7aee9e0d6e2b6a814cdf99045e49
Eyerusalem-birhanu <jerrybirhanu94@gmail.com>
2022-06-02 11:24:32 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-*   main -it prints "and that piece of art is useful"
-*   - Dora Korpar, 2015-10-19",
-*   followed by a new line, to the standard error.
-*   Return: 0 on right
+* main -it prints "and that piece of art is useful"
+* - Dora Korpar, 2015-10-19",
+* followed by a new line, to the standard error.
+* Return: 0 on right
 */
 int main(void)
 {
