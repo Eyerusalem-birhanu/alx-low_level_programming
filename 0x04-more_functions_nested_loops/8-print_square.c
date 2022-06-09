74d455d1c9711653adf56e80c830c077dba8b6f5
Eyerusalem-birhanu <jerrybirhanu94@gmail.com>
2022-06-09 20:23:19 +0300

    print numbers

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,29 +2,24 @@
 
 /**
  * print_square - check for a digit
- * @n : number to be printed
+ * @n : The number of lines using '#' characters to use per row and column
  * Return: Void
  */
 
 void print_square(int n)
 
 {
-
 	int i = 0, ii;
 	while (i < n && n > 0)
-
 	{
 		ii = 0;
 		while (ii < n)
-
 		{
 			_putchar('#');
 			ii++;
-
 		}
 		_putchar('\n');
 		i++;
-
 	}
 	if (i == 0)
 		_putchar('\n');
