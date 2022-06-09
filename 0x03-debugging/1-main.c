4cb9ea8ef895d4dac878034a4bf36de29075e0b3
Eyerusalem-birhanu <jerrybirhanu94@gmail.com>
2022-06-09 20:10:53 +0300

    infinity loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,6 +10,7 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
 
 	/**
@@ -19,6 +20,5 @@ int main(void)
 	*}
 	*/
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
