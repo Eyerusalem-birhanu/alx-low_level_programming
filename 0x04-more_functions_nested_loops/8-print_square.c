1c114e7e0e70c24b5abdbe73076cfc08ac4f0872
Eyerusalem-birhanu <jerrybirhanu94@gmail.com>
2022-06-09 20:19:02 +0300

    print numbers

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,6 +12,7 @@ void print_square(int n)
 
 	int i = 0, ii;
 	while (i < n && n > 0)
+
 	{
 		ii = 0;
 		while (ii < n)
@@ -22,6 +23,7 @@ void print_square(int n)
 		_putchar('\n');
 		i++;
 	}
+	
 	if (i == 0)
 		_putchar('\n');
 }
