e45bc8b23849d13a39df64e23c84e6a53e241aff
Eyerusalem-birhanu <jerrybirhanu94@gmail.com>
2022-06-30 17:37:12 +0300

    allocate memory

--- school.sh
+++ school.sh
@@ -1,29 +1,13 @@
-#!/bin/bash 
-echo "School"
-https://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@github.com/Eyerusalem-birhanu/alx-zero_day.git
-git config --global user.email
 
-
-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
-git clone https://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/Eyerusalem-birhanu/alx-system_engineering-devops.git
-git clone https://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/Eyerusalem-birhanu/alx-system_engineering-devops.git
-git config --global --unset http.proxy
-https://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@github.com/Eyerusalem-birhanu/alx-system_engineering-devops.git
-https://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@github.com/Eyerusalem-birhanu/alx-system_engineering-devops.git
-#!/bin/bash
 git config --global user.email "jerrybirhanu94@gmail.com"
 git config --global user.name "Eyerusalem-birhanu"
-git commit -m "T1"
-https://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@github.com/Eyerusalem-birhanu/zero_day.git
-https://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@github.com/Eyerusalem-birhanu/alx-zero_day.git
-https://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@github.com/Eyerusalem-birhanu/alx-system_engineering-devops.git
-https://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@github.com/Eyerusalem-birhanu/alx-system_engineering-devops.git
-#!/bin/bash
-printf "%o\n" $(( (5#$(echo $WATER | tr '[water]' '[01234]')) + (5#$(echo $STIR | tr '[stir.]' '[01234]')) )) | tr '[01234567]' '[bestchol]'
-echo $(printf %o $(($((5#$(echo $WATER | tr 'water' '01234'))) + $((5#$(echo $STIR | tr 'stir.' '01234'))))) | tr '01234567' 'behlnort')
-https://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@github.com/Eyerusalem-birhanu/alx-low_level_programming.git
 
-https://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@github.com/Eyerusalem-birhanu/alx-low_level_programming.git
-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     new token
 
-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
+
+
+
+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             token
+
+gcc -c *.c
+
+ar rcs libmy.a *.o
